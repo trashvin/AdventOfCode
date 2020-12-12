fd7a8c7e557bf5faa401833d831525ba57016954
trashvin <m3lles@gmail.com>
2020-12-12 18:11:03 +0800

    day11

---
+++ 2020/csharp/day10/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace day10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> adapters = new List<int>();
+
+            //get the numbers
+            foreach(string adapter in File.ReadLines("test.txt"))
+            {
+                adapters.Add(Int32.Parse(adapter));
+            }
+
+            //sanity check
+            Console.WriteLine(adapters[adapters.Count-1]);
+
+            // long result1 = SolvePart1(adapters);
+            // Console.WriteLine($"Part1 result = {result1}");
+
+            long result2 = SolvePart2(adapters);
+            Console.WriteLine($"Part2 result = {result2}");
+        }
+
+        static int SolvePart1(List<int> origAdapters)
+        {
+            List<int> adapters = new List<int>();
+            adapters.AddRange(origAdapters);
+            adapters.Add(0);
+            adapters.Sort();
+            PrintList(adapters);
+            int by1 = 0;
+            int by3 = 1;
+            for(int i = 0 ; i < adapters.Count-1; i++)
+            {
+                //Console.Write($"{adapters[i]} ; {adapters[i+1]}");
+                if (adapters[i+1] - adapters[i] == 1)
+                {
+                    //Console.WriteLine(" b1");
+                    by1++;
+                }
+                else
+                {
+                    //Console.WriteLine(" b3");
+                    by3++;
+                }
+            }
+            //Console.WriteLine($"By1 {by1} ; By3 {by3}");
+            return by1*by3;
+        }
+
+        static int SolvePart2(List<int> origAdapters)
+        {
+            List<int> adapters = new List<int>();
+            adapters.AddRange(origAdapters);
+            adapters.Add(0);
+            adapters.Sort();
+            PrintList(adapters);
+            
+            int count = 1; 
+            
+            int i=0;
+            List<int> accumulator = new List<int>();
+            int skip = 1;
+            while(i<adapters.Count-3)
+            {
+                var segment = adapters.Skip(skip).Take(3).ToArray();
+                for(int inner = 2 ; inner >=0 ; inner--)
+                {
+                    int diff = segment[inner] - adapters[i];
+
+                    if ( diff <= 3)
+                    {
+                        i = i + diff;
+                        skip += diff;
+                        count++;
+                        break;
+                    }
+                }
+
+                
+               
+
+            }
+
+            return count;
+        }
+        static void PrintList(List<int> codes)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("----------------------------");
+            foreach(int i in codes)
+            {
+                Console.Write(i.ToString() + " , ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("----------------------------");
+        }
+    }
+}
---
+++ 2020/csharp/day11/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace day11
+{
+    class Program
+    {
+        const char EMPTY = 'L';
+        const char OCCUPIED = '#';
+        const char FLOOR = '.';
+        static int ROW_COLS = 10;
+        static void Main(string[] args)
+        {
+            List<string> rowOfSeats = new List<string>();
+
+            //get the numbers
+            foreach(string rowSeat in File.ReadLines("input.txt"))
+            {
+                rowOfSeats.Add(rowSeat);
+            }
+
+            //sanity check
+            //Console.WriteLine(rowOfSeats[rowOfSeats.Count-1]);
+
+            //PrintList(rowOfSeats);
+            ROW_COLS = rowOfSeats[0].Length;
+            long result1 = SolvePart1(rowOfSeats);
+            Console.WriteLine($"Part1 result = {result1}");
+
+            // long result2 = SolvePart2(adapters);
+            // Console.WriteLine($"Part2 result = {result2}");
+        }
+
+        static int SolvePart1(List<string> rowOfSeats)
+        {
+            int noOfOccupied = 0;
+            bool withMovements = true;
+            //PrintList(rowOfSeats);
+            List<string> origRowOfSeats = new List<string>();
+            origRowOfSeats.AddRange(rowOfSeats);
+
+            int currentRow = 0;
+            while(withMovements)
+            {
+                currentRow = 0;
+                int movements =0;
+                foreach(string rowSeat in rowOfSeats)
+                {
+                    //Console.WriteLine(rowSeat);
+                    StringBuilder newRow = new StringBuilder();
+                    char[] temp = rowSeat.ToArray();
+                    for(int i=0; i<rowSeat.Length; i++)
+                    {
+                        
+                        if(rowSeat[i] == EMPTY)
+                        {
+                            if(!HasAdjacent(rowOfSeats,currentRow,i,true))
+                            {
+                                temp[i] = OCCUPIED;
+                                origRowOfSeats[currentRow] = new string(temp);
+                                movements++;
+
+                            }
+                            //PrintList(origRowOfSeats);
+                            //Console.ReadLine();
+                        }
+                        else if(rowSeat[i] == OCCUPIED)
+                        {
+                            if(HasAdjacent(rowOfSeats,currentRow,i,false))
+                            {
+                                temp[i] = EMPTY;
+                                origRowOfSeats[currentRow] = new string(temp);
+                                movements++;
+                            }
+                        }
+                        else
+                        {
+                            temp[i] = rowSeat[i];
+                            origRowOfSeats[currentRow] = new string(temp);
+                        }
+                        //Console.ReadLine();
+                    }
+                    //Console.WriteLine("777" + newRow.ToString());
+                    //origRowOfSeats[currentRow]=newRow.ToString();
+                    
+                    currentRow ++;
+                }
+                PrintList(origRowOfSeats);
+                rowOfSeats.Clear();
+                rowOfSeats.AddRange(origRowOfSeats);
+                if (movements == 0)
+                {
+                    break;
+                }
+            }
+            
+            foreach(string row in rowOfSeats)
+            {
+                noOfOccupied += row.Count(x => x == OCCUPIED);
+            }
+
+            //PrintList(newRowOfSeats);
+            return noOfOccupied;
+        }
+
+        static bool HasAdjacent(List<string> rowOfSeats, int currentRow, int currentCol, bool isEmpty)
+        {
+            bool result = false;
+            int empty = 0;
+            
+            for(int row = currentRow-1; row<currentRow+2; row++)
+            {
+                for(int col = currentCol -1; col<currentCol+2; col++)
+                {
+                    if(row>=0 && row<rowOfSeats.Count)
+                    {
+                        if(col>=0 && col<ROW_COLS)
+                        {
+                            if(row==currentRow && col==currentCol)
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                if(rowOfSeats[row][col]==EMPTY || rowOfSeats[row][col]==FLOOR)
+                                {
+                                    empty++;
+                                }
+                            }
+                        } else empty++;
+                    }
+                    else empty++;
+                }
+
+            }
+
+            if (isEmpty)
+            {
+                if (empty == 8) result = false;
+                else result = true;
+            }
+            else
+            {
+                if(empty<=4) result = true;
+                else result = false;
+            }
+            
+          
+            return result;
+        }
+        //static 
+        static void PrintList(List<string> seats)
+        {
+            Console.WriteLine("*********************");
+            foreach(string seat in seats)
+            {
+                Console.WriteLine(seat);
+            }
+        }
+    }
+}
