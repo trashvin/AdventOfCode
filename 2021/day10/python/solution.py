7fa83efa4d29d1cf4cf6b76c9b098b326563d1d2
trashvin <m3lles@gmail.com>
2021-12-13 23:28:34 +0800

    day 10 python

--- 2021/day10/python/solution.py
+++ 2021/day10/python/solution.py
@@ -1,8 +1,8 @@
 import timeit
 import numpy as np
-from collections  import defaultdict
+from collections  import defaultdict, deque
 
-DO_TEST = 1
+DO_TEST = 0
 
 def read_file(filename):
     data = []
@@ -10,25 +10,95 @@ def read_file(filename):
         data_string = file.read()
         data_string = data_string.split('\n')
     
-    data = []
-    for line in data_string:
-        row = list(line)
-        data.append(row)
-    
-    
-    return data
+    return data_string
 
 def do_part1(file):
     data = read_file(file)
-    
-    result = 0
+
+    """ scoring
+    ): 3 points.
+    ]: 57 points.
+    }: 1197 points.
+    >: 25137 points.
+    """
+    scoring = dict()
+    scoring[')'] = 3
+    scoring[']'] = 57
+    scoring['}'] = 1197
+    scoring['>'] = 25137
+
+    opens = ['{','<','(','[']
+    closes = ['}','>',')',']']
+    accumulator = 0
+    for line in data:
+        syntax = list(line)
+        stack = deque()
+        error = False
+        for symbol in syntax:
+            if symbol in opens:
+                stack.append(symbol)
+            else:
+                out_val = stack.pop()
+                if opens.index(out_val) != closes.index(symbol):
+                    error = True
+                    break;
+
+        if error:
+            print("adding : ",scoring[symbol])
+            accumulator += scoring[symbol]
+
+        error = False
+
+    result = accumulator
     print(f"part 1 = {result}")
     
 
 def do_part2(file):
     data = read_file(file)
     
-    result = 0
+    """ scoring
+    ): 1 points.
+    ]: 2 points.
+    }: 3 points.
+    >: 4 points.
+    """
+    scoring = dict()
+    scoring[')'] = 1
+    scoring[']'] = 2
+    scoring['}'] = 3
+    scoring['>'] = 4
+
+    opens = ['{','<','(','[']
+    closes = ['}','>',')',']']
+    accumulator = 0
+    scores = []
+    for line in data:
+        syntax = list(line)
+        stack = deque()
+        error = False
+        for symbol in syntax:
+            if symbol in opens:
+                stack.append(symbol)
+            else:
+                out_val = stack.pop()
+                if opens.index(out_val) != closes.index(symbol):
+                    error = True
+                    break;
+
+        if error:
+            error = False
+            continue
+        
+        score = 0
+        while len(stack) >0:
+            completer = stack.pop()
+            completer = closes[opens.index(completer)]
+            score = score * 5 + scoring[completer]
+        
+        scores.append(score)
+
+    scores.sort()
+    result = scores[int(len(scores)/2)]
     print(f"part 2 = {result}")
 
 
