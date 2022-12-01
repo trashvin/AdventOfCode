fa5a03b878e9a13002518a2bc8b04d987789c326
trashvin <m3lles@gmail.com>
2022-12-01 15:35:18 +0800

    day01 solution

--- 2022/day01/Program.cs
+++ 2022/day01/Program.cs
@@ -1,2 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using helper;
+using System.Runtime.InteropServices;
+
+List<string> input = Helper.ReadList<string>("data.txt");
+Dictionary<int,int> caloriesPerElf = new Dictionary<int, int>();
+
+int elfNumber = 0;
+int tempTotalCal = 0;
+int highestElf = 0;
+int highestCal = 0;
+
+//part 1
+
+foreach(var calorie in input)
+{
+    if (!String.IsNullOrWhiteSpace(calorie))
+    {
+        tempTotalCal += Int32.Parse(calorie);
+    }
+    else 
+    {
+        caloriesPerElf.Add(elfNumber, tempTotalCal);
+
+        if(tempTotalCal > highestCal)
+        {
+            highestCal = tempTotalCal;
+            highestElf = elfNumber;
+        }
+
+        elfNumber++;
+        tempTotalCal = 0;
+    }
+}
+
+Helper.WriteResult(1, highestCal);
+int totalCalOfThreeElf = 0;
+// start of part 2
+foreach(KeyValuePair<int,int> elf in caloriesPerElf.OrderByDescending(key=>key.Value).Take(3))
+{
+    Console.WriteLine($"{elf.Key} : {elf.Value}");
+    totalCalOfThreeElf += elf.Value;
+}
+
+Helper.WriteResult(2,totalCalOfThreeElf);
--- 2022/helper/helper.cs
+++ 2022/helper/helper.cs
@@ -1,7 +1,44 @@
 ﻿namespace helper
 {
-    public class helper
+    public static class Helper
     {
+        public static void WriteResult(int part, long value)
+        {
+            Console.WriteLine($"Part {part} result = {value}");
+        }
+        public static void WriteResult(int part, int value)
+        {
+            Console.WriteLine($"Part {part} result = {value}");
+        }
+        public static List<T> ReadList<T>(string fileName)
+        {
+            List<T> list = new List<T>();
 
+            foreach (string line in File.ReadLines(fileName))
+            {
+                list.Add((T)Convert.ChangeType(line, typeof(T)));
+            }
+
+            return list;
+        }
+        public static void PrintList<T>(List<T> list, bool oneLine = false)
+        {
+            Console.WriteLine("----------");
+            foreach (T line in list)
+            {
+                if (oneLine) Console.Write(line);
+                else Console.WriteLine(line);
+            }
+            Console.WriteLine("----------");
+        }
+        //public static void PrintDict<T, U>(Dictionary<T, U> dict)
+        //{
+        //    Console.WriteLine("----------");
+        //    foreach (KeyValuePair<T, U> kvp in dict)
+        //    {
+        //        Console.WriteLine($"{kvp.Key} : {kvp.Value}");
+        //    }
+        //    Console.WriteLine("----------");
+        //}
     }
 }
