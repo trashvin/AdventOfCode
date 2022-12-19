ee2bc0a4a4eedefe8f50709dadd3b67d9cbfefe8
trashvin <m3lles@gmail.com>
2022-12-19 14:47:45 +0800

    day10

--- 2022/day10/Program.cs
+++ 2022/day10/Program.cs
@@ -1,6 +1,6 @@
 ﻿using helper;
 
-List<string> input = Helper.ReadList<string>("test.txt");
+List<string> input = Helper.ReadList<string>("data.txt");
 
 List<int> cycles = new List<int>();
 
@@ -31,5 +31,46 @@ var result = (20 * cycles[19])
 Helper.WriteResult(1, result.ToString());
 
 // part 2
+var cycleCount = 0;
+var addCycle = 0;
+var position = 1;
+var output = "";
+foreach (string line in input)
+{
+    if (line.StartsWith("noop"))
+    {
+        cycleCount++;
+        output += AppendPixel(cycleCount, position);
 
+        if (cycleCount % 40 == 0) output += "\n";
+    }
+    else
+    {
+        int addVal = Int32.Parse(line.Substring(4));
+        while (addCycle<2)
+        {
+            cycleCount++;
+            output += AppendPixel(cycleCount, position);
+           
+            if (addCycle == 1) position += addVal;
+            
+            addCycle++;
+
+            if (cycleCount % 40 == 0) output += "\n";
+        }
+
+        addCycle = 0;
+    }
+}
+
+Helper.WriteResult(2, "\n" + output);
+
+string AppendPixel(int cycle, int position)
+{
+    if ((cycle - 1) % 40 == position - 1 || (cycle - 1) % 40 == position || (cycle - 1) % 40 == position + 1)
+        return "#";
+    else
+        return ".";
+  
+}
 
